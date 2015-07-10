f36eb05db87a14c7193fc947f396394448de1001
Carl Halliburton <carl.halliburton@paradise.net.nz>
2015-07-10 13:04:17 +1200

    fixed problems, program now only converts binary to decimal

--- src/numberbaseconvertor/NumberBaseConvertor.java
+++ src/numberbaseconvertor/NumberBaseConvertor.java
@@ -18,8 +18,7 @@ public class NumberBaseConvertor
     private String outPut;
     private ArrayList<Integer> intPartList;
     private ArrayList<Integer> decPartList;
-    private ArrayList<String> hexOctList;
-    private HashMap<String, String> hexMap;
+
    
     /**
      * Constructor for objects of class BaseConvertor
@@ -28,9 +27,6 @@ public class NumberBaseConvertor
     {
         intPartList = new ArrayList<>();
         decPartList = new ArrayList<>();
-        hexOctList = new ArrayList<>();
-        hexMap = new HashMap<>();
-        fillHexMap();
         
         outPut = null;
         inPut = null;
@@ -59,16 +55,6 @@ public class NumberBaseConvertor
         return outPut;
     }
     
-    public void fillHexMap()
-    {
-        hexMap.put("A", "10");
-        hexMap.put("B", "11");
-        hexMap.put("C", "12");
-        hexMap.put("D", "13");
-        hexMap.put("E", "14");
-        hexMap.put("F", "15");
-    }
-    
     public void baseToDecimal( int base )
     {
         String intPart;
@@ -93,32 +79,11 @@ public class NumberBaseConvertor
         int exponent = intPart.length() - 1;
         int digit = 0;
         String hexTemp;
-           
-        if (base == 16) {
-            for ( int i = 0; i < intPart.length(); i++ ) {
-                hexOctList.add("" + intPart.charAt( i )); 
-            }
-            for ( int i = 0; i < intPart.length(); i++ ) {
-                hexTemp = hexOctList.get(i);
-                if (hexOctList.get(i).equals("A") ||
-                    hexOctList.get(i).equals("B") ||
-                    hexOctList.get(i).equals("C") ||
-                    hexOctList.get(i).equals("D") ||
-                    hexOctList.get(i).equals("E") ||
-                    hexOctList.get(i).equals("F") ) {
-                    hexOctList.add(i, hexMap.get(hexTemp));
-                }
-                intPart = intPart.concat(hexOctList.get(i));
-            }   
-            System.out.print("InrPar - " + intPart);
-            
-        }
 
         for ( int i = 0; i < intPart.length(); i++ ) {
             intPartList.add( Character.getNumericValue( intPart.charAt( i ) ) ); 
         }
-        
-    /*    
+
         for ( int i = 0; i < intPart.length(); i++ ) {
             digit = intPartList.get( i );
             if ( digit == 0 ) {
@@ -131,8 +96,6 @@ public class NumberBaseConvertor
             exponent--;
         }
         outPutStr = Integer.toString( outPutNum );
-        */
-        outPutStr = intPart;
         return outPutStr;
     }
     
@@ -142,7 +105,7 @@ public class NumberBaseConvertor
         int digit = 0;
         String outPutStr = "";
         int exponent = 1;
-       /* 
+     
         for ( int i = 0; i < decPart.length(); i++ ) {
             decPartList.add( Character.getNumericValue( decPart.charAt( i ) ) ); 
         }
@@ -159,7 +122,7 @@ public class NumberBaseConvertor
         }
         outPutStr = String.valueOf( outPutNum );
         outPutStr = outPutStr.substring( outPutStr.indexOf( "." ) + 1, outPutStr.length() );
-        */
+
         return outPutStr;
     }
     
